0380c919ddfe23be829e7fd7568851192ba471fb
Kalingaraj29 <Kalingaraj127@gmail.com>
2023-06-16 16:43:43 +0530

    Final commit
    Final commit

---
+++ firebase/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAYpLerrWfc0bl270B-pCRJs84ywS_cbmo",
+  authDomain: "videochallenge-b9400.firebaseapp.com",
+  projectId: "videochallenge-b9400",
+  storageBucket: "videochallenge-b9400.appspot.com",
+  messagingSenderId: "275539982125",
+  appId: "1:275539982125:web:8a02a1715c72979c0c45a4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/Pages/Video.js
+++ src/Pages/Video.js
@@ -1,24 +1,66 @@
 import React, { useState } from "react";
 import ReactPlayer from "react-player";
-import download from "./download.jpg";
-import download1 from "./download (1).jpg";
+import download1 from "./Assets/1.jpg";
+import download2 from "./Assets/2.jpg";
+import download3 from "./Assets/3.jpg";
+import download4 from "./Assets/4.jpg";
+import download5 from "./Assets/5.jpg";
+import download6 from "./Assets/6.jpg";
+import download8 from "./Assets/8.jpg";
+import download9 from "./Assets/9.jpg";
 import "../App.css";
 
 const Video = () => {
   const Data = [
     {
       url: "https://www.youtube.com/watch?v=7WXUjcbaHfo",
-      thumbail: download,
+      thumbnail: download1,
     },
     {
-      url: "https://www.youtube.com/watch?v=_DWLy9fFh5U",
-      thumbail: download1,
+      url: "https://www.youtube.com/watch?v=ANzPM5-lwXc&list=PLu0W_9lII9aiQiOwthuSvinxoflmhRxM3",
+      thumbnail: download2,
+    },
+    {
+      url: "https://www.youtube.com/watch?v=ZezXKK-Q7hE&list=PLyYcNnaAVG5K7mTlMiRNwKiJrgRWsuiyY",
+      thumbnail: download3,
+    },
+    {
+      url: "https://www.youtube.com/watch?v=L4_jarMnB0c&list=PLu0W_9lII9ahwFDuExCpPFHAK829Wto2O",
+      thumbnail: download4,
+    },
+    {
+      url: "https://www.youtube.com/watch?v=7dmtMcYGmxU&list=PLucuzpTBeckvKLLJOFoiaacdJN4owWi7a",
+      thumbnail: download5,
+    },
+    {
+      url: "https://www.youtube.com/watch?v=PSGdZsKVSf4&list=PL7BQ4lqtgECRG2oWnM-m5d3oyoZ8zwbD2&index=1",
+      thumbnail: download6,
+    },
+    {
+      url: "https://www.youtube.com/watch?v=7dmtMcYGmxU&list=PLucuzpTBeckvKLLJOFoiaacdJN4owWi7a",
+      thumbnail: download1,
+    },
+    {
+      url: "https://www.youtube.com/watch?v=PSGdZsKVSf4&list=PL7BQ4lqtgECRG2oWnM-m5d3oyoZ8zwbD2&index=1",
+      thumbnail: download3,
+    },
+    {
+      url: "https://www.youtube.com/watch?v=pFvWwFua6mw",
+      thumbnail: download8,
+    },
+    {
+      url: "https://www.youtube.com/watch?v=7WXUjcbaHfo",
+      thumbnail: download9,
+    },
+    {
+      url: "https://www.youtube.com/watch?v=PSGdZsKVSf4&list=PL7BQ4lqtgECRG2oWnM-m5d3oyoZ8zwbD2&index=1 ",
+      thumbnail: download1,
     },
   ];
 
-  const [video1, setvideo1] = useState(Data[0].url);
+  const [video, setvideo] = useState(Data[4].url);
   const handleclick = (url) => {
-    setvideo1(url);
+    setvideo(url);
   };
 
   const [hover, setHover] = useState(false);
@@ -28,23 +70,25 @@ const Video = () => {
   const handlemouseleave = () => {
     setHover(false);
   };
+
   return (
     <div className="ontainer">
-      <ReactPlayer className="player" url={video1} controls />
-      <button onMouseOver={handlemouseover} className="thumbnail_button">
-        Thumbnail
-      </button>
-      <div className="thumbnails-container" onMouseLeave={handlemouseleave}>
+      <ReactPlayer className="player" url={video} controls  onMouseOver={handlemouseover} onMouseLeave={handlemouseleave} />
+      <div className="circleborder"  onMouseOver={handlemouseover}>
         {hover &&
-          Data.map((el, index) => (
-            <img
-              key={index}
-              src={el.thumbail}
-              alt={`Thumbnail ${index}`}
-              className="thumbnail"
-              onClick={() => handleclick(el.url)}
-            />
-          ))}
+          Data?.map((el, index) => {
+            return (
+              <div className="divcircle" key={index} onMou>
+                <img
+                  key={index}
+                  src={el.thumbnail}
+                  alt={`Thumbnail ${index}`}
+                  className="thumbnail"
+                  onClick={() => handleclick(el.url)}
+                />
+              </div>
+            );
+          })}
       </div>
     </div>
   );
